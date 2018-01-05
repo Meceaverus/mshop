7d761cc15ba77aea6fecf51fd8c99294acb2a755
Meceaverus <Meceaverus22@gmail.com>
2018-01-05 22:57:15 +0200

    lesson18

--- src/Controller/About.php
+++ src/Controller/About.php
@@ -8,16 +8,36 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class About extends Controller
 {
     /**
      *
-     * @Route("/mshop/about")
+     * @Route("/about", name="about_show")
      */
-    public function showAbout(){
-        return $this->render('mshop/about.html.twig');
+    public function show(SessionInterface $session){
+
+        $url = $this->generateUrl('category_show', [
+            'slug' => 'notebooks',
+            'param' => 'getparam',
+        ]); UrlGeneratorInterface::ABSOLUTE_URL;
+
+        return $this->render('about/show.html.twig', [
+            'notebooksUrl' => $url,
+            'lastCategory' => $session->get('lastVisitedCategory')
+        ]);
+    }
+
+    /**
+     * @Route("/to-about")
+     */
+    public function redirectToShow()
+    {
+        return $this->redirectToRoute('about_show');
     }
 }
