faf61a8ce9d4a282eb48583102fd3b0d082017d3
Meceaverus <Meceaverus22@gmail.com>
2018-01-09 21:35:18 +0200

    LESSON26

--- src/Controller/About.php
+++ src/Controller/About.php
@@ -8,7 +8,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Feedback;
+use App\Form\FeedbackType;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -40,4 +43,19 @@ class About extends Controller
     {
         return $this->redirectToRoute('about_show');
     }
+
+    /**
+     * @Route("/feedback", name="feedback")
+     *
+     * @param Request $request
+     */
+    public function  feedback(Request $request)
+    {
+        $form = $this->createForm(FeedbackType::class);
+
+        return $this->render('about/feedback.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
 }
---
+++ src/Form/FeedbackType.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Olf
+ * Date: 09.01.2018
+ * Time: 21:08
+ */
+
+namespace App\Form;
+
+use App\Entity\Feedback;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class FeedbackType extends AbstractType
+{
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => Feedback::class,
+        ));
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('email')
+            ->add('message')
+            ;
+    }
+
+
+}
